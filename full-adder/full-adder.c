b07f92a982c65d84ecf671b22b2bdccc7b9ccfe0
clara-tyman2 <clara.tyman@stu.ocsb.ca>
2025-06-02 10:02:30 -0400

    full adder start of code

---
+++ full-adder/full-adder.c
@@ -0,0 +1,48 @@
+// Created on: June 2025
+// Created by: Clara T
+// Created for: The code for a full adder
+
+const int DIGIT_A1 = 13;
+const int DIGIT_B1 = 12;
+const int DIGIT_CIN = 7;
+
+void setup()
+{
+  // setup serial output
+  Serial.begin(9600);
+  // set pin mode to output
+  pinMode(DIGIT_A1, OUTPUT);
+  pinMode(DIGIT_B1, OUTPUT);
+  pinMode(DIGIT_CIN, OUTPUT);
+  
+  // set all pins to "0)
+  digitalWrite(DIGIT_A1, 0); //LOW and HIGH just reference 0 & 1
+  digitalWrite(DIGIT_B1, 0);
+  digitalWrite(DIGIT_CIN, 0);
+  
+  // loop over the inputs
+  Serial.print("Starting ... \n\n");
+  delay(1000); // Wait for 1000 millisecond(s)
+
+  for (int onesA = 0; onesA <= 1; ++onesA) {
+    digitalWrite(DIGIT_A1, onesA);
+    for (int onesB = 0; onesB <= 1; ++onesB) {
+      digitalWrite(DIGIT_B1, onesB);
+      for (int onesCIN = 0; onesCIN <= 1; ++onesCIN) {
+        digitalWrite(DIGIT_CIN, onesCIN);
+      
+      Serial.print("A1 = " + String(onesA) + "\n");
+      Serial.print("B1 = " + String(onesB) + "\n");
+      Serial.print(String(onesA) + " + " + String(onesB) + " = " + String(onesA + onesB) + "\n\n");
+      delay(2000); // Wait for 2000 millisecond(s)
+      }
+    }
+  }
+}
+
+void loop()
+{
+  // Done
+  Serial.print("\nDone. \n");
+  delay(10000); // Wait for 10000 millisecond(s)
+}
