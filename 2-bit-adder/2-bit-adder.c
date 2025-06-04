0724f762f6e48a9f1f8d04adf6b2c101a0dbd2b5
clara-tyman2 <clara.tyman@stu.ocsb.ca>
2025-06-04 10:01:23 -0400

    2 bit adder

--- 2-bit-adder/2-bit-adder.c
+++ 2-bit-adder/2-bit-adder.c
@@ -1,40 +1,51 @@
+// Created by:  Clara T
 // Created on: June 2025
-// Created by: Clara T
-// Created for: The code for a 2 bit adder
+//
+// This is the code for a 2-bit adder.
 
-const int CIN = 13;
-const int DIGIT_A1 = 12;
+const int DIGIT_A1 = 13;
+const int DIGIT_A2 = 12;
 const int DIGIT_B1 = 11;
+const int DIGIT_B2 = 10;
+const int CIN = 9;
 
 void setup()
 {
   // setup serial output
   Serial.begin(9600);
   // set pin mode to output
-  pinMode(CIN, OUTPUT);
   pinMode(DIGIT_A1, OUTPUT);
+  pinMode(DIGIT_A2, OUTPUT);
   pinMode(DIGIT_B1, OUTPUT);
+  pinMode(DIGIT_B2, OUTPUT);
+  pinMode(CIN, OUTPUT);
   
   // set all pins to "0"
-  digitalWrite(CIN, 0); //LOW and HIGH just reference 0 & 1
-  digitalWrite(DIGIT_A1, 0); 
-  digitalWrite(DIGIT_B1, 0);
+  digitalWrite(DIGIT_A1, 0); //LOW and HIGH just reference 0 & 1
+  digitalWrite(DIGIT_A2, 0); 
+  digitalWrite(DIGIT_B1, 0); 
+  digitalWrite(DIGIT_B2, 0);
+  digitalWrite(CIN, 0); 
   
   // loop over the inputs
   Serial.print("Starting ... \n\n");
   delay(1000); // Wait for 1000 millisecond(s)
 
-  for (int cin = 0; cin <= 1; ++cin) {
-    digitalWrite(CIN, cin);
+  for (int twosA = 0; twosA <= 2; twosA = twosA + 2) {
+    digitalWrite(DIGIT_A2, twosA);
     for (int onesA = 0; onesA <= 1; ++onesA) {
       digitalWrite(DIGIT_A1, onesA);
-      for (int onesB = 0; onesB <= 1; ++onesB) {
-        digitalWrite(DIGIT_B1, onesB);
-          Serial.print("Cin = " + String(cin) + "\n");
+      for (int twosB = 0; twosB <= 2; twosB = twosB +2) {
+        digitalWrite(DIGIT_B2, twosB);
+        for (int onesB = 0; onesB <= 1; ++onesB) {
+          digitalWrite(DIGIT_B1, onesB);
+          Serial.print("A2 = " + String(twosA) + "\n");
           Serial.print("A1 = " + String(onesA) + "\n");
+          Serial.print("B2 = " + String(twosB) + "\n");
           Serial.print("B1 = " + String(onesB) + "\n");
-          Serial.print(String(cin) + " + " + String(onesA) + " + " + String(onesB) + " = " + String(cin + onesA + onesB) + "\n\n");
-          delay(3500); // Wait for 2000 millisecond(s)
+          Serial.print(String(twosA + onesA) + " + " + String(twosB + onesB) + " = " + String(twosA + onesA + twosB + onesB) + "\n\n");
+          delay(4000); // Wait for 2000 millisecond(s)
+        }
       }
     }
   }
